222446edefa416de0d60f5c3695671368ca82d7b
masahiro29 <38945284+masahiro29@users.noreply.github.com>
2022-04-23 23:33:54 +0900

    first commit

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return '<h1>Hello World!!!!</h1>'
+   return '<h1>Hello World!!!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=port)
